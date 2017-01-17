7a6d5f635b876d918540e067e2ff54ed77d9ce5e
Jared Fuller <jaredshane@gmail.com>
2017-01-17 15:43:02 -0600

    added some basic text in partials and got an ajax call to my firebase database data to console log

--- app.js
+++ app.js
@@ -3,13 +3,62 @@ const app = angular.module('MyApp', ['ngRoute'])
 app.config(($routeProvider, $locationProvider) => {
   $locationProvider.hashPrefix('')
   $routeProvider
+    .when('/', {
+      controller: 'MainCtrl',
+      templateUrl: 'partials/main.html'
+    })
     .when('/register',{
       controller: 'RegisterCtrl',
       templateUrl: 'partials/register.html'
     })
+    .when('/login', {
+      controller: 'LoginCtrl',
+      templateUrl: 'partials/login.html'
+    })
+    .when('/new', {
+      controller: 'NewCtrl',
+      templateUrl: 'partials/new.html'
+    })
+    .when('/notes', {
+      controller: 'NotesCtrl',
+      templateUrl: 'partials/notes.html'
+    })
+    .otherwise ({
+      redirectTo: '/'
+    })
 })
 
+app.controller('MainCtrl', function ($scope) {
+  console.log('MainCtrl')
+})
 
 app.controller('RegisterCtrl', function ($scope) {
-  
+  console.log('RegisterCtrl')
 })
+
+app.controller('LoginCtrl', function ($scope) {
+  console.log('LoginCtrl')
+})
+
+app.controller('NewCtrl', function ($scope) {
+  console.log('NewCtrl')
+})
+
+app.controller('NotesCtrl', function ($scope, $http) {
+  console.log("NotesCtrl");
+  $http.get(`https://mf-user-notes-daf3d.firebaseio.com/.json`)
+    .then((val) => {
+      console.log(val.data)
+    })
+})
+
+
+
+// app.controller('ListCtrl', function ($scope, $http){
+//   console.log('ListCtrl')
+//   $http.get(`list.json`)
+//       .then((val) => {
+//         console.log("list.json", val.data)
+//         $scope.list = val.data.list
+//       })
+// })
